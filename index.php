8251e73cb77a2505619009045da18af9d48cc6ae
rohullahayobi <rohullahayobi@gmail.com>
2020-05-08 15:20:50 +0430

    login entry

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="refresh" content="0;url=public/login.php">
+    <title>SB Admin</title>
+    <script language="javascript">
+        window.location.href = "public/login.php"
+    </script>
+</head>
+
+<body>
+    Go to <a href="public/login.php">/public/login.php</a>
+</body>
+
+</html>
